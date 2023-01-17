3e37b06844167b462261c02ce336b6fc6ebe33a0
snowBro <jaco.f168@gmail.com>
2023-01-17 13:09:42 +0200

    update home page

--- ansible/roles/home-page/tasks/nginx/www/script.js
+++ ansible/roles/home-page/tasks/nginx/www/script.js
@@ -1,165 +1,79 @@
 const IP = window.location.hostname
 const URL = `http://${IP}:4200`
+let theme = "dark"
+
+const DARK_MODE = {
+    "--boxShadow": "rgba(0, 0, 0, 0.3)",
+    "--bodyBg": "rgba(0, 0, 0, 0.774)",
+    "--hover":  "#2196F3",
+    "--active":  "#2cb2ff",
+    "--server": "#00000050",
+    "--serverBorder": "#000000",
+    "--serverTitle": "rgba(255, 255, 255, 0.75)",
+}
+
+const LIGHT_MODE = {
+    "--boxShadow": "rgba(0, 0, 0, 0.3)",
+    "--bodyBg": "rgb(0 0 0 / 0%)",
+    "--hover":  "#000000bd",
+    "--active":  "#000000",
+    "--server": "#2196F3",
+    "--serverBorder": "#2196F3",
+    "--serverTitle": "rgb(0 0 0 / 75%)",
+}
 
 const redirect = (path) => {
     window.location.href = `/${path}`;
+}
+
+const redirectNewTab = (path) => {
+    window.open(`${IP}/${path}`, "_blank");
 }
 
 const showState = () => {
     document.getElementById('states').style.display = "flex";
 }
 
-const set_info = (name, data) => {
-    document.getElementById(`${name}Active`).textContent = data['count'];
-    document.getElementById(`${name}Unknown`).textContent = data['unknownCount'];
-    if(data.error || data.unknownErrors) {
-        document.getElementById(`server-${name}`).classList.add("server-error");
-        return
-    }
-    if(data.warnings || data.unknownWarnings) {
-        document.getElementById(`server-${name}`).classList.add("server-warning");
-        return
-    }
-    document.getElementById(name).style.display = "block";
-}
 
-const set_server_info = (data) => {
-    showState()
-    const temp = data['temp']
-    const tempValue = document.getElementById("tempValue")
-    document.getElementById("fanValue").checked = data['fan'] === "ON" ? true : false;
-    tempValue.textContent = temp;
-    tempValue.style.color = "#13c713";
-    if (temp >= 50) {
-        tempValue.style.color = "red";
-    } else if (temp >= 40) {
-        tempValue.style.color = "yellow";
-    }
 
-}
-
-const getServerState = async () => {
-    try {
-        const response = await fetch(`${URL}/info`);
-        // waits until the request completes...
-        if (response && response.status === 200) {
-            let stateData = await response.json();
-            if (stateData) {
-                set_server_info(stateData)
-            }
-            return
-        } else {
-            console.error("Error in getting server state data");
-        }
-    } catch (error) {
-        console.error("Unable to get server state data");
-    }
-}
-
-const get_sonarr_info = async () => {
-    const response = await fetch(`${URL}/queue/sonarr`);
-    if (response && response.status === 200) {
-        let jsonResponse = await response.json();
-        if (jsonResponse) {
-            set_info('sonarr', jsonResponse)
-        }
-        return
+const toggleTheme = () => {
+    if (theme === "dark") {
+        loadTheme(LIGHT_MODE)
+        theme = "light"
     } else {
-        console.error("Error in getting sonarr info");
-        document.getElementById('sonarr').style.display = "none";
+        loadTheme(DARK_MODE)
+        theme = "dark"
+    }
+    setThemeImg()
+    window.localStorage.setItem('theme', theme);
+}
+
+
+const loadTheme = (themeCss) => {
+    const root = document.querySelector(':root');
+    for (const iterator in themeCss) {
+        root.style.setProperty(iterator, themeCss[iterator]);
     }
 }
 
-const get_radarr_info = async () => {
-    const response = await fetch(`${URL}/queue/radarr`);
-    if (response && response.status === 200) {
-        let jsonResponse = await response.json();
-        if (jsonResponse) {
-            set_info('radarr', jsonResponse)
-        }
+const setThemeImg = () => {
+    const element = document.getElementById("theme-img")
+    if (theme === "dark") { 
+        element.src = "./assets/sun.png"
         return
+    }
+    element.src = "./assets/moon.png"
+}
+
+if (window.localStorage.getItem('theme')) {
+    theme = window.localStorage.getItem('theme');
+    if (theme === "dark") { 
+        loadTheme(DARK_MODE)
     } else {
-        console.error("Error in getting radarr info");
-        document.getElementById('radarr').style.display = "none";
+        loadTheme(LIGHT_MODE)
     }
+} else {
+    loadTheme(DARK_MODE)
 }
 
-const change_fan_state = async () => {
-    let fanState = document.getElementById("fanValue").checked ? 'ON' : "OFF";
-    const response = await fetch(`${URL}/fan/${fanState}`);
-    if (response && response.status === 200) {
-        let jsonResponse = await response.text();
-        console.info(jsonResponse);
-        return
-    } else {
-        console.error("Error changing fan state");
-    }
-}
-
-const build_queue = (serverName, jsonResponse) => {
-    // <tr>
-    //     <td class="title">Title</td>
-    //     <td>Size</td>
-    //     <td>TimeLeft</td>
-    // </tr>
-    const id = `${serverName}Queue`
-    for (const item of jsonResponse) {
-        const tr = document.createElement('tr');
-
-        let td = document.createElement('td');
-        td.className = "title"
-        td.textContent = item["title"]
-        tr.appendChild(td)
-
-        td = document.createElement('td');
-        td.textContent = item["size"]
-        tr.appendChild(td)
-
-        td = document.createElement('td');
-        td.textContent = item["timeleft"]
-        tr.appendChild(td)
-   
-        document.getElementById(id).getElementsByTagName("tbody")[0].appendChild(tr)
-    }
-
-}
-
-const get_sonarr_queue = async () => {
-    const response = await fetch(`${URL}/queue/details/sonarr`);
-    if (response && response.status === 200) {
-        let jsonResponse = await response.json();
-        // console.info(jsonResponse);
-        build_queue("sonarr", jsonResponse)
-        return
-    } else {
-        console.error("Error changing fan state");
-    }
-}
-
-const get_radarr_queue = async () => {
-    const response = await fetch(`${URL}/queue/details/radarr`);
-    if (response && response.status === 200) {
-        let jsonResponse = await response.json();
-        build_queue("radarr", jsonResponse)
-        return
-    } else {
-        console.error("Error changing fan state");
-    }
-}
-
-const setImageUrl = () => {
-    const imageContainers = document.getElementsByClassName("server-img");
-    for (const img of imageContainers) {
-        img.src = img.src.replace("%5Eip%5E", IP)
-        console.log(img.src)
-    }
-}
-
-setImageUrl();
-if (false) {
-    getServerState();
-    get_sonarr_info();
-    get_radarr_info();
-    get_sonarr_queue();
-    get_radarr_queue();
-}
+setThemeImg()
